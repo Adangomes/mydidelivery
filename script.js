3f8b7946996c9f5dd4e4f6df991426900f70b065
Adan Gomes <adangones55@gmail.com>
2026-02-09 17:27:40 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const GEOAPIFY_KEY = "208f6874a48c45e68761f3d994db6775";
 const RESTAURANTE_COORD = [-49.024909, -26.464334];
 const TAXA_BASE = 5;
 const VALOR_POR_KM = 1.5;
-const WHATSAPP_NUMERO = "5547984196636";
+const WHATSAPP_NUMERO = "5547992745867";
 
 let carrinho = [];
 let produtos = [];
@@ -732,6 +732,7 @@ function voltarParaDados() {
     document.getElementById("resumo-pedido").style.display = "none";
     document.getElementById("form-entrega").style.display = "block";
 }
+
 
 
 
