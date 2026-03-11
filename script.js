58cc6268d5311feebcc1095747d623d54ac071cd
Adan Gomes <adangones55@gmail.com>
2026-03-10 22:28:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const GEOAPIFY_KEY = "208f6874a48c45e68761f3d994db6775";
 
-const RESTAURANTE_COORD = [-26.462521, -49.022055]; 
+const RESTAURANTE_COORD = [-26.472038, -48.997615]; 
 
 const TAXA_BASE = 5;
 
@@ -887,4 +887,5 @@ function salvarPedidoFirebase(dados) {
     });
 
 }
+
 
