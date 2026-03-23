0caad4eddcafd3d083f946ff76c3b13e8ab7a450
Adan Gomes <adangones55@gmail.com>
2026-03-23 10:21:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -413,7 +413,7 @@ function voltarParaEntrega() {
 
 
 // --- NOVAS CONFIGURAÇÕES DE HORÁRIO ---
-const H_ABERTURA = 10;
+const H_ABERTURA = 18;
 const M_ABERTURA = 00; // 18:00
 const H_FECHAMENTO = 22;
 const M_FECHAMENTO = 30; // 22:30
